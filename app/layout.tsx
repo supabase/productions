b905abeadc813baee050960ed99af2f33b763ea4
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-12-06 17:03:07 +0100

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,26 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
+import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Supabase Album',
   description: 'Listen to Supabase themed synth-wave bombs.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en">
       <head>
         <link rel="icon" href="favicon.ico" sizes="any" />
       </head>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
+      <body className="bg-background text-foreground">{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,86 @@
-import SupabaseLogo from '../components/SupabaseLogo'
+import Image from 'next/image';
+import SupabaseLogo from '../components/SupabaseLogo';
+import Link from 'next/link';
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center h-16">
-        <div className="w-full max-w-4xl flex justify-center items-center p-3 text-sm">
-          <SupabaseLogo />
-        </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex justify-center flex-col gap-20 opacity-0 max-w-4xl px-3">
-          player
+    <div className="opacity-0 animate-in p-[20px] pb-0 flex-1 w-full h-screen flex flex-col items-center overflow-hidden max-h-screen">
+      <div className="relative w-[calc(100vw-40px)] h-[calc(100vh-20px-100px)] rounded-lg overflow-hidden">
+        {/* <Image
+          src="/images/supabase-album-cover.jpg"
+          alt="Supabase album steam-punk art"
+          fill
+          className="absolute inset-0 object-cover z-10"
+          draggable={false}
+        /> */}
+        <iframe
+          src="https://www.youtube-nocookie.com/embed/[YT_ID_HERE]?autoplay=1&enablejsapi=1&widgetid=0playsinline=0&&muted=1&showinfo=0&controls=0&fs=0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          placeholder="/images/supabase-album-cover.jpg"
+          allowFullScreen={false}
+          className="absolute inset-0 w-full h-full"
+        />
       </div>
 
-      <footer className="w-full p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{' '}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
+      <div className="relative h-[100px] flex gap-20 w-full p-[20px] items-center justify-center md:justify-between">
+        <nav className="hidden md:block">
+          <Link
+            href="https://supabase.com/launch-week"
+            className="w-auto py-2 flex gap-3 justify-center items-center hover:opacity-80 transition-opacity"
           >
-            Supabase
-          </a>
-        </p>
-      </footer>
+            <SupabaseLogo />
+            <span className="tracking-[2px] uppercase">Album</span>
+          </Link>
+        </nav>
+
+        <div className="flex gap-4 items-center text-foreground">
+          <span className="text-sm">Listen on</span>
+          <SocialLink
+            title="YouTube"
+            href="https://www.youtube.com/"
+            img="/icons/youtube.svg"
+          />
+          <SocialLink
+            title="Spotify"
+            href="https://www.youtube.com/"
+            img="/icons/spotify.svg"
+          />
+          <SocialLink
+            title="Apple Music"
+            href="https://www.youtube.com/"
+            img="/icons/apple.svg"
+          />
+          <SocialLink
+            title="Bandcamp"
+            href="https://www.youtube.com/"
+            img="/icons/bandcamp.svg"
+          />
+          <SocialLink
+            title="SoundCloud"
+            href="https://www.youtube.com/"
+            img="/icons/soundcloud.svg"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+const SocialLink = ({
+  title,
+  href,
+  img,
+}: {
+  title: string;
+  href: string;
+  img: string;
+}) => (
+  <Link
+    href={href}
+    className="w-[50px] h-[50px] flex items-center justify-center rounded-full bg-white border border-transparent hover:shadow-xl hover:border-btn-background transition-all"
+    title={title}
+    target="_blank"
+  >
+    <Image src={img} alt={`${title} logo"`} width={26} height={26} />
+  </Link>
+);
