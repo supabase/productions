1dfeb68aebbb7242773d75fa4ce87a91e2a0a359
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-12-07 15:36:36 +0100

    meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,25 @@
 import './globals.css';
+import type { Metadata } from 'next';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000';
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Supabase Album',
-  description: 'Listen to Supabase themed synth-wave bombs.',
+  description:
+    'Supabase SynthWave. The best soundtrack to build an app in a weekend and scale to billions.',
+  keywords: ['synthwave', 'supabase', 'ai', 'soundtrack'],
+  openGraph: {
+    title: 'Supabase Album',
+    url: 'https://supabase.productions/',
+    images: ['/images/supabase-album-cover.png']
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
